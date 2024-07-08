d1afc0d115dec9cf06c0ad51de4dcc7a2233aa9c
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-08 17:51:11 +0530

    Data Axis With Argument

---
+++ React/Argument-props/App.jsx
@@ -0,0 +1,20 @@
+// ================ React Components ===============
+
+import Employee from "./Employee";
+import Student from "./Student";
+
+const nm="Sachin Mishra";
+const cls="Mtech";
+const fees=45000;
+
+const App=()=>{
+  return(
+    <>
+      <h1>
+      </h1>
+     {/* <Employee nm="Rahul" empno="E062" sal="50000"/> */}
+     <Student nm={nm} cls={cls} stufees={fees}/>
+    </>
+  )
+}
+export default App;
