cfd30ff8b48be40d53803eca53b6483ddef33c18
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-14 10:59:21 +0530

    Map-Method

--- React/Map-Method/App.jsx
+++ React/Map-Method/App.jsx
@@ -1,8 +1,30 @@
-const name=["Ram","Syam","Mohan","Sohan"];
-const ans=name.map((key)=>{
+// const name=["Ram","Syam","Mohan","Sohan"];
+// const ans=name.map((key)=>{
+//   return(
+//     <>
+//     <h1>{key}</h1>
+//     </>
+//   )
+// })
+
+// const App=()=>{
+  
+//   return(
+//     <>
+//      <h1>Welcome to cybrom </h1>
+//      {ans}
+//     </>
+//   )
+// }
+// export default App;
+
+//===========================================
+
+const age=[24,20,6,8,10,13];
+const ans=age.map((key)=>{
   return(
     <>
-    <h1>{key}</h1>
+    <h1>{key*2}</h1>
     </>
   )
 })
