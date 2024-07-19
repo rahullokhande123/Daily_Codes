b500ebadff9acd996d8531b3e6a00819f3573ed2
Rahul Lokhande <rlokhande06244@gmail.com>
2024-07-19 16:08:33 +0530

    Inset Contact

---
+++ React/Test/Contact.jsx
@@ -0,0 +1,23 @@
+// import { useState } from "react";
+// const App=()=>{
+
+//   const [cnt,setCnt]=useState(0)
+  
+//   const Inc=()=>{
+//     setCnt(cnt+15)
+//   }
+//   const Dec=()=>{
+//     setCnt(cnt-15)
+//   }
+
+//   return(
+//     <>
+//     <h1>Wellcome Counter Program</h1>
+//     <h1>{cnt}</h1>
+//     <button onClick={Inc}>Incriment</button>
+//     <button onClick={Dec}>Decriment</button>
+    
+//     </>
+//   )
+// }
+// export default App;
