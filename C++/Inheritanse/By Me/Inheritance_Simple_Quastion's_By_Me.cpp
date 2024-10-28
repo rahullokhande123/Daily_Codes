8dce4f8eabde5b0eb73a2c528f4778673c4aa81b
Rahul Lokhande <rlokhande06244@gmail.com>
2024-10-28 23:04:49 +0530

    Inheritance

--- C++/Inheritanse/By Me/Inheritance_Simple_Quastion's_By_Me.cpp
+++ C++/Inheritanse/By Me/Inheritance_Simple_Quastion's_By_Me.cpp
@@ -83,44 +83,44 @@ int main(){
 
 //====================== Qustion 3 By Normal Inheritance ======================
 
-//#include <iostream>
-//using namespace std;
-//
-//
-//class BankAccount{
-//	protected:
-//		int amount,deposit,withdraw;
-//	public:
-//	   BankAccount(int a,int d,int w){
-//	     	amount=a;
-//		    deposit=d;
-//		    withdraw=w;
-//	}	
-//};
-//class SavingAccount:public BankAccount{
-//	public:
-//	   SavingAccount(int a,int d,int w):BankAccount(a,d,w){    
-//	}
-//	void bankdetails(){
-//		cout<<"Amount"<<amount<<endl;
-//		cout<<"Deposit"<<deposit<<endl;
-//		cout<<"Withdraw"<<withdraw<<endl;
-//	}
-//};
-//class CheckingAccount:public SavingAccount{
-//	public:
-//       CheckingAccount(int a,int d,int w):SavingAccount(a,d,w){    
-//	}
-//	void balance(){
-//		cout<<"Total Amount:"<<amount+deposit-withdraw;
-//	}
-//};
-//int main(){
-//	
-//	CheckingAccount c(10000,5000,2000);
-//
+#include <iostream>
+using namespace std;
+
+
+class BankAccount{
+	protected:
+		int amount,deposit,withdraw;
+	public:
+	   BankAccount(int a,int d,int w){
+	     	amount=a;
+		    deposit=d;
+		    withdraw=w;
+	}	
+};
+class SavingAccount:public BankAccount{
+	public:
+	   SavingAccount(int a,int d,int w):BankAccount(a,d,w){    
+	}
+	void bankdetails(){
+		cout<<"Amount"<<amount<<endl;
+		cout<<"Deposit"<<deposit<<endl;
+		cout<<"Withdraw"<<withdraw<<endl;
+	}
+};
+class CheckingAccount:public SavingAccount{
+	public:
+      CheckingAccount(int a,int d,int w):SavingAccount(a,d,w){    
+	}
+	void balance(){
+		cout<<"Total Amount:"<<amount+deposit-withdraw;
+	}
+};
+int main(){
+	
+	CheckingAccount c(10000,5000,2000);
+
+	c.bankdetails();
+	c.balance();
 //	c.bankdetails();
-//	c.balance();
-////	c.bankdetails();
-//}
+}
 
