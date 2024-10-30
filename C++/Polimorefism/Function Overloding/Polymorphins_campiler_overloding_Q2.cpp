4b45aabe344e7097b26cc5ffb834199cdf981079
Rahul Lokhande <rlokhande06244@gmail.com>
2024-10-30 17:57:31 +0530

    Polimorphism_F()OverLoding

--- C++/Polimorefism/Function Overloding/Polymorphins_campiler_overloding_Q2.cpp
+++ C++/Polimorefism/Function Overloding/Polymorphins_campiler_overloding_Q2.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-using namespace std;
-class Addition{
-	private:
-		int n1,n2;
-	public:
-		Addition(int a, int b){
-			n1=a;
-			n2=b;
-		}
-	void add_with_outsider(int a){
-		cout<<a+n1+n2<<endl;
-	}
-	void add_with_outsider(double a){
-		cout<<a+n1+n2<<endl;
-	}
-};
-int main(){
-	Addition obj(2,3);
-	obj.add_with_outsider(78.9);
-}
+// #include <iostream>
+// using namespace std;
+// class Addition{
+// 	private:
+// 		int n1,n2;
+// 	public:
+// 		Addition(int a, int b){
+// 			n1=a;
+// 			n2=b;
+// 		}
+// 	void add_with_outsider(int a){
+// 		cout<<a+n1+n2<<endl;
+// 	}
+// 	void add_with_outsider(double a){
+// 		cout<<a+n1+n2<<endl;
+// 	}
+// };
+// int main(){
+// 	Addition obj(2,3);
+// 	obj.add_with_outsider(78.9);
+// }
 
 //using namespace std;
 //class Addition{
