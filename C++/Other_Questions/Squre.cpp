3bbf0ce282ecd690f2b1bcaa97c668849c34cc80
Rahul Lokhande <rlokhande06244@gmail.com>
2024-11-22 23:48:01 +0530

    Abstraction_Questions

---
+++ C++/Other_Questions/Squre.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int l,b,square;
+	cout<<"Enter the value of length:";
+	cin>>l;
+	cout<<"Enter the value of breath:";
+	cin>>b;
+	square=l*b;
+	cout<<"Value of square:"<<square; // Value of square=168
+	
+	return 0;
+}
