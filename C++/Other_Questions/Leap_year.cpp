1441c8ad46cbff2fdcf0a8155b50b961204652a6
Rahul Lokhande <rlokhande06244@gmail.com>
2024-11-24 23:47:58 +0530

    Leap_Year_Programe

--- C++/Other_Questions/Leap_year.cpp
+++ C++/Other_Questions/Leap_year.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
-using namespace std;
-int main(){
-	int year;
-	cout<<"enter test year:";
-	cin>>year;
-	if((year%4==0 && year%100!=0) || year%400==0 ){
-		cout<<"year is a leap yeaer";
-	}
-	else{
-		cout<<"year is not a leap year  ";
-	}	
-}
+// #include <iostream>
+// using namespace std;
+// int main(){
+// 	int year;
+// 	cout<<"enter test year:";
+// 	cin>>year;
+// 	if((year%4==0 && year%100!=0) || year%400==0 ){
+// 		cout<<"year is a leap yeaer";
+// 	}
+// 	else{
+// 		cout<<"year is not a leap year  ";
+// 	}	
+// }
 
 //#include <iostream>
 //using namespace std;
