e924c6393d6adce59ac927d2a31968f564c4a721
Rahul Lokhande <rlokhande06244@gmail.com>
2024-11-24 23:45:11 +0530

    Switch_Case

--- C++/Other_Questions/Switch_Case.cpp
+++ C++/Other_Questions/Switch_Case.cpp
@@ -22,3 +22,28 @@ int main(){
 	    default:cout<<"invalid choice";				      
 }
 }
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int weeks;
+//	cout<<"enter number:";
+//	cin>>weeks;
+//	switch(weeks){
+//		case 1: cout<<"Monday";
+//		    break;
+//		case 2: cout<<"Tuesday";
+//		    break;
+//		case 3: cout<<"Wednusday";
+//		    break;
+//		case 4: cout<<"Thusrsday";
+//		    break;
+//	    case 5: cout<<"Friday";
+//	        break;
+//	    case 6: cout<<"Saturday";
+//	        break;
+//	    case 7:  cout<<"Sunday";
+//	        break;
+//	    default: cout<<"Invalide Number:";
+//	}
+//}
