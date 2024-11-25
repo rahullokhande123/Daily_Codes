d299330c9a1933f92140e37d3889f81f2e7473f2
Rahul Lokhande <rlokhande06244@gmail.com>
2024-11-25 23:23:35 +0530

    Even_Odd_Programe

---
+++ C++/Other_Questions/Even_Odd.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num;
+	cout<<"enter number:";
+	cin>>num;
+	if(num%7==0){
+		cout<<num<<"  "<<"is divisible by 7 \n";
+		if (num%2==0)
+		cout<<num<<"  "<<"is a even number";
+		else
+		cout<<num<<"  "<<"is a odd number";
+	}
+	else{
+		cout<<num<<"  "<<"is not divisible by 7 \n";
+		if(num%2==0)
+		   cout<<num<<"  "<<"is a even number";
+		else 
+		   cout<<num<<"  "<<"is a odd number ";
+    }
+    
+}
