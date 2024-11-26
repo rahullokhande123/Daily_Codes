064a75460fa16122fd6601df5104b662d2dd90ce
Rahul Lokhande <rlokhande06244@gmail.com>
2024-11-27 00:01:29 +0530

    Switch_Statment

--- C++/Other_Questions/Switch_Case.cpp
+++ C++/Other_Questions/Switch_Case.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
-using namespace std;
-int main(){
-	int weekN;
-	cout<<"enter week number:";
-	cin>>weekN;
-	switch(weekN){
-	case 1: cout<<"Monday";
-		        break;
-    case 2: cout<<"Tuysday";
-		        break;
-	case 3: cout<<"Wednesday";
-		        break;  
-    case 4: cout<<"Thursday";
-		        break;	
-	case 5: cout<<"Friday";
-		        break;
-	case 6: cout<<"Saturday";
-		        break;
-    case 7: cout<<"Sunday";
-		        break;	
-	    default:cout<<"invalid choice";				      
-}
-}
+// #include <iostream>
+// using namespace std;
+// int main(){
+// 	int weekN;
+// 	cout<<"enter week number:";
+// 	cin>>weekN;
+// 	switch(weekN){
+// 	case 1: cout<<"Monday";
+// 		        break;
+//     case 2: cout<<"Tuysday";
+// 		        break;
+// 	case 3: cout<<"Wednesday";
+// 		        break;  
+//     case 4: cout<<"Thursday";
+// 		        break;	
+// 	case 5: cout<<"Friday";
+// 		        break;
+// 	case 6: cout<<"Saturday";
+// 		        break;
+//     case 7: cout<<"Sunday";
+// 		        break;	
+// 	    default:cout<<"invalid choice";				      
+// }
+// }
 
 //#include <iostream>
 //using namespace std;
