acac52e9fb9672230dbc77a09e30af9a29ba3a15
Rahul Lokhande <rlokhande06244@gmail.com>
2024-11-28 23:05:25 +0530

    Armstrong_Numbers

--- C++/Other_Questions/ArmstrongNo.cpp
+++ C++/Other_Questions/ArmstrongNo.cpp
@@ -37,3 +37,34 @@ int main(){
 //	    cout<<"not Armstrong";
 //}
 
+
+#include <iostream>
+using namespace std;
+int main(){
+	int num,temp,sum=0,last;
+	cout<<"Enter test no. of 3 digit:";
+	cin>>num;
+	temp=num;
+	while(num>0){
+		last=num%10; // 37.1 --> Ekai ka ank 1 le lenge
+		sum=sum+(last*last*last); // sum=0+(1*1*1=3),-->sum=3
+		num=num/10; // 371/10 krenge to--> 37.1 hoga 
+		            // jisme hm point ke bad ki value nhi lenge
+		            // sirf 37 hi lenge
+		            // ab (num=37)
+		            // ab 3 time loop chalega or jab 0 aa jaye tb 
+		            // loop terminet ho jayega or last me 
+		            // 371 hi nikal ke aayega (it is Armstrong Number)
+	}
+	if(temp==sum)
+	     cout<<"Yes.... It is a Armstrong no.";
+	else 
+	     cout<<"Sorry.... It is not Armstrong no.";   
+}
+
+
+
+
+
+
+
