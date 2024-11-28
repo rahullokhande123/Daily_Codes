9d9480d6cf50b38e45e8272a301b56ed6897c12e
Rahul Lokhande <rlokhande06244@gmail.com>
2024-11-28 23:05:48 +0530

    Armstrong_Numbers

--- C++/Other_Questions/ArmstrongNo.cpp
+++ C++/Other_Questions/ArmstrongNo.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
-using namespace std;
-int main(){
-	int num,temp,sum=0,last;
-	cout<<"Enter test no. of 3 digit:";
-	cin>>num;
-	temp=num;
-	while(num>0){
-		last=num%10;
-		sum=sum+(last*last*last); // 1^3
-		num=num/10;
+// #include <iostream>
+// using namespace std;
+// int main(){
+// 	int num,temp,sum=0,last;
+// 	cout<<"Enter test no. of 3 digit:";
+// 	cin>>num;
+// 	temp=num;
+// 	while(num>0){
+// 		last=num%10;
+// 		sum=sum+(last*last*last); // 1^3
+// 		num=num/10;
 		
-	}
-	if(temp==sum)
-	     cout<<"Armstrong Number:";
-	else 
-	     cout<<"No it is not Armstrong Number:";   
-}
+// 	}
+// 	if(temp==sum)
+// 	     cout<<"Armstrong Number:";
+// 	else 
+// 	     cout<<"No it is not Armstrong Number:";   
+// }
 
 
 //#include <iostream>
