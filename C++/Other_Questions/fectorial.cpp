25b070b5ef22bf8e269775e8f56746a5dec819a0
Rahul Lokhande <rlokhande06244@gmail.com>
2024-11-30 23:22:10 +0530

    Factorial_Programe

--- C++/Other_Questions/fectorial.cpp
+++ C++/Other_Questions/fectorial.cpp
@@ -16,3 +16,22 @@
 //	}
 //	cout<<fact;
 //}
+
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int num,fect=1;
+//	cout<<"enter Number:";
+//	cin>>num;
+//	cout<<num<<"!=";
+//	for(int i=num;i>=1;i--){
+//		if(i==1){
+//			cout<<i<<"=";
+//		}
+//		else
+//		   cout<<i<<"*"; 
+//		fect=fect*i;
+//	}
+//	cout<<fect;
+//}
