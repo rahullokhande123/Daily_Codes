30000aa7f46e1b27b589d7f5d84da31632c6359d
Rahul Lokhande <rlokhande06244@gmail.com>
2024-12-01 21:40:02 +0530

    For_Loop

--- C++/Other_Questions/for_loop.cpp
+++ C++/Other_Questions/for_loop.cpp
@@ -36,3 +36,26 @@
 //	}
 //	cout<<sum;
 //}
+
+
+// ==================================================
+
+#include <iostream>
+using namespace std;
+int main(){
+	int n,sum=0;
+	cout<<"Enter No.";
+	cin>>n;
+	for(int i=1;i<=n;i++){
+	
+		
+		if(i==n)
+			cout<<"("<<i<<"+"<<i<<")"<<"=";
+		else
+		    	cout<<"("<<i<<"+"<<i<<")"<<"+";
+		
+		sum=i+i+sum;
+	}
+
+	    cout<<sum;
+}
