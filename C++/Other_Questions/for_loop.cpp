74d0b573a7b33ec306960bbd7d2247370be16460
Rahul Lokhande <rlokhande06244@gmail.com>
2024-12-01 21:41:33 +0530

    For_Loop

--- C++/Other_Questions/for_loop.cpp
+++ C++/Other_Questions/for_loop.cpp
@@ -40,22 +40,35 @@
 
 // ==================================================
 
-#include <iostream>
-using namespace std;
-int main(){
-	int n,sum=0;
-	cout<<"Enter No.";
-	cin>>n;
-	for(int i=1;i<=n;i++){
+// #include <iostream>
+// using namespace std;
+// int main(){
+// 	int n,sum=0;
+// 	cout<<"Enter No.";
+// 	cin>>n;
+// 	for(int i=1;i<=n;i++){
 	
 		
-		if(i==n)
-			cout<<"("<<i<<"+"<<i<<")"<<"=";
-		else
-		    	cout<<"("<<i<<"+"<<i<<")"<<"+";
+// 		if(i==n)
+// 			cout<<"("<<i<<"+"<<i<<")"<<"=";
+// 		else
+// 		    	cout<<"("<<i<<"+"<<i<<")"<<"+";
 		
-		sum=i+i+sum;
-	}
+// 		sum=i+i+sum;
+// 	}
 
-	    cout<<sum;
-}
+// 	    cout<<sum;
+// }
+
+// =================== Pettern By For Loop ==================
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	for(int i=5;i>=1;i--){
+//		for(int j=1;j<=i;j++){
+//			cout<<"*";
+//		}
+//		cout<<endl;
+//	}
+//}
