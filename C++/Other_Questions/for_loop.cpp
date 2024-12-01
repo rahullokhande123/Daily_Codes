50f4af6cce8825b9aeb02b6e7412bd02ebaf48ac
Rahul Lokhande <rlokhande06244@gmail.com>
2024-12-01 21:42:49 +0530

    For_Loop

--- C++/Other_Questions/for_loop.cpp
+++ C++/Other_Questions/for_loop.cpp
@@ -72,3 +72,23 @@
 //		cout<<endl;
 //	}
 //}
+
+// =================== Nested Pettern By For Loop =================
+
+#include <iostream>
+using namespace std;
+int main(){
+	for(int i=0;i<=5;i++){
+		for(int j=0;j<i;j++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	for(int i=5;i>=0;i--){
+		for(int j=i;j>=0;j--){
+			cout<<"*";
+		}
+		cout<<endl;
+		
+	}
+}
