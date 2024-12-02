4de88c5459da1a7df3af2fa68e09c0f3a8370d83
Rahul Lokhande <rlokhande06244@gmail.com>
2024-12-02 23:49:40 +0530

    Pattern_By_ForLoop

--- C++/Other_Questions/for_loop.cpp
+++ C++/Other_Questions/for_loop.cpp
@@ -92,3 +92,18 @@ int main(){
 		
 	}
 }
+
+// =====================================================================
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	char ch='A';
+//	for(int i=1;i<=4;i++){
+//		for(int j=1;j<=5;j++){
+//			cout<<ch;
+//			ch++;
+//			}
+//		cout<<endl;
+//	}
+//}
