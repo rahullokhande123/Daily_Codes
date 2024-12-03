b013d23d4b7b83ec1293c79b2e729ca79f783b7a
Rahul Lokhande <rlokhande06244@gmail.com>
2024-12-03 22:56:00 +0530

    For_Loop_Range

--- C++/Other_Questions/for_loop.cpp
+++ C++/Other_Questions/for_loop.cpp
@@ -123,20 +123,22 @@ int main(){
 //	
 //}
 
-#include <iostream>
-using namespace std;
-int main(){
-	char ch='A';
-	for(int i=1;i<=5;i++){
-		for(int j=1;j<=5;j++){
-			cout<<ch;
-			ch++;
-		}
+// ============================================
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+// 	char ch='A';
+// 	for(int i=1;i<=5;i++){
+// 		for(int j=1;j<=5;j++){
+// 			cout<<ch;
+// 			ch++;
+// 		}
 		
-		cout<<"\n";
-	}
+// 		cout<<"\n";
+// 	}
 	
-}
+// }
 
 // =========================================
 
