97c7048444fc4858fda5e7de3ec4dae5e434da16
Rahul Lokhande <rlokhande06244@gmail.com>
2024-12-06 17:24:06 +0530

    Sum_Program

--- C++/Other_Questions/Sum.cpp
+++ C++/Other_Questions/Sum.cpp
@@ -29,3 +29,26 @@ int main(){
   }
   cout<<"Sum:"<<sum<<endl;
 } 
+
+
+// =====================================================
+
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	double sum=0;
+	cout<<"Enter No.";
+	cin>>n;
+	for(int i=1;i<=n;i++){ // 6
+		
+		if(i==n)
+			  cout<<1<<"/"<<i<<"=";
+		else
+		    cout<<1<<"/"<<i<<"+";
+		sum=sum+(1.0/i);	
+	}
+	cout<<sum;
+}
+
+
