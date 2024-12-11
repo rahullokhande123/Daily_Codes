483f8619ddf4922d0fabe508496af524b7bfe0e6
Rahul Lokhande <rlokhande06244@gmail.com>
2024-12-11 17:07:11 +0530

    Arrar_Practice

---
+++ C++/Other_Questions/Array_Practice.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int ar[5]={1,2,4,55,6};
+//	cout<<ar[8] // junk value print kr dega 
+//	
+//	cout<<"enter number:";
+//	cin>>ar[30] // undefind behaviar show karega.
+//}
+
+// WAP to create a array of size n, 
+// after creation,search a element ask by a user, if search operation pass return true,
+// otherwise False.
