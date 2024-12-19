170401a4bfe5576ecbfc24f07e5868fb9c455e24
Rahul Lokhande <rlokhande06244@gmail.com>
2024-12-19 22:37:10 +0530

    Array_Question

--- C++/Other_Questions/Array_Q_Inline.cpp
+++ C++/Other_Questions/Array_Q_Inline.cpp
@@ -21,3 +21,20 @@ using namespace std;
 		cout<<ar[i]<<" ";
 	}
 }
+//int main(){
+//	int ar[2][3];
+//	for(int i=0;i<2;i++){
+//		for(int j=0;j<3;j++){
+//			cout<<"enter number:";
+//			cin>>ar[i][j];
+//		}
+//	}
+//	cout<<"IN Metrix Fom\n";
+//	for(int i=0;i<2;i++){
+//		for(int j=0;j<3;j++){
+//			cout<<ar[i][j]<<" ";
+//		}
+//		cout<<endl;
+//	}
+//	
+//}
