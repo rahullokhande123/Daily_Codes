ec798dd1b5ce1f10b0313ced6a89da3a136ff51b
Rahul Lokhande <rlokhande06244@gmail.com>
2025-01-08 23:02:57 +0530

    Deep Copy Constructor

---
+++ C++/Other_Questions/Deep_Copy_Constructor.cpp
@@ -0,0 +1,20 @@
+// ===================== Copy Cunstructor In Heap =====================
+//#include <iostream>
+//using namespace std;
+//
+//class Student{
+//	private:
+//		string name;
+//		int age,*marks;
+//		
+//	public:
+//		Student(string n,int a,int m){
+//			name=n;
+//			age=a;
+//			marks=new int;
+//			*marks=m;
+//		}
+//	  
+//	  void display(){
+//	  	cout<<"Name:"<<name<<" age:"<<age<<" marks:"<<*marks<<endl;
+//	  }
